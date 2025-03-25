f51cb9c7d7c11d38ff400e45c282d141db615110
Ying-Kai Liao <lykevin890919@gmail.com>
2025-03-25 17:28:14 +0800

    Initial commit

---
+++ app/components/LiffProvider.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { useState, useEffect, createContext, useContext } from "react";
+import type { Liff } from "@line/liff";
+import type { ReactNode } from "react";
+
+// Create a context for LIFF
+type LiffContextType = {
+  liff: Liff | null;
+  liffError: string | null;
+};
+
+const LiffContext = createContext<LiffContextType>({
+  liff: null,
+  liffError: null,
+});
+
+export const useLiff = () => useContext(LiffContext);
+
+export default function LiffProvider({ children }: { children: ReactNode }) {
+  const [liffObject, setLiffObject] = useState<Liff | null>(null);
+  const [liffError, setLiffError] = useState<string | null>(null);
+
+  // Execute liff.init() when the app is initialized
+  useEffect(() => {
+    // to avoid `window is not defined` error
+    import("@line/liff")
+      .then((liff) => liff.default)
+      .then((liff) => {
+        console.log("LIFF init...");
+        liff
+          .init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID! })
+          .then(() => {
+            console.log("LIFF init succeeded.");
+            setLiffObject(liff);
+          })
+          .catch((error: Error) => {
+            console.log("LIFF init failed.");
+            setLiffError(error.toString());
+          });
+      });
+  }, []);
+
+  return (
+    <LiffContext.Provider value={{ liff: liffObject, liffError }}>
+      {children}
+    </LiffContext.Provider>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+import LiffContent from "./components/LiffContent";
+
+export default function Home() {
+  return <LiffContent />;
+}
