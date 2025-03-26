03568add5046d3f2735af58af0a812d94de95444
Ying-Kai Liao <lykevin890919@gmail.com>
2025-03-26 16:21:06 +0800

    eslint rule updated

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: [
-    'next/core-web-vitals',
-  ],
-  rules: {
-    // Disable the unused variables rule entirely
-    '@typescript-eslint/no-unused-vars': 'off'
-  }
-};
